466e1f31bf6dcbc745e9a6ae5ce773cd17d8ff5f
Joy Kumar Acharjee <joy29940@gmail.com>
2025-06-19 16:49:40 +0600

    feat: update path

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Setting(BaseSettings):
     POSTGRES_URL: str
 
-    model_config = SettingsConfigDict(env_file="../.env")
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 Config = Setting()
